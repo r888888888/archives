5a0edef17dee4e0ad2b3ba34bbaaa1b80bfe0aba
Albert Yi <ayi@desk.com>
2017-11-02 13:54:02 -0700

    fix typo

--- script/setup/build_archives.sh
+++ script/setup/build_archives.sh
@@ -2,5 +2,5 @@
 
 docker container prune
 docker image prune
-docker build -t r888888888/archives -f scripts/setup/Dockerfile.archives .
+docker build -t r888888888/archives -f script/setup/Dockerfile.archives .
 docker push r888888888/archives
